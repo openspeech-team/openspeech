2e037fe18d6cb63a79ec0e7b680d62f17d8bd067
hasangchun <seomk9896@naver.com>
2021-08-24 23:55:30 +0900

    Resolved #76

---
+++ tests/test_acoustic_models/test_conv2d_subsampling.py
@@ -0,0 +1,16 @@
+import unittest
+import logging
+
+from openspeech.modules import Conv2dSubsampling
+from openspeech.utils import DUMMY_INPUTS, DUMMY_INPUT_LENGTHS
+
+logger = logging.getLogger(__name__)
+
+
+class TestConformer(unittest.TestCase):
+    def test_conv2d_subsampling(self):
+        conv_subsample = Conv2dSubsampling(input_dim=80, in_channels=1, out_channels=512)
+
+        outputs, output_lengths = conv_subsample(DUMMY_INPUTS, DUMMY_INPUT_LENGTHS)
+        print(f"input lengths : {DUMMY_INPUT_LENGTHS}")
+        print(f"output lengths : {output_lengths}")

7650e35f49f707aac74dd02f1f71b8baa30c5516
Sangchun Ha (Patrick) <seomk9896@naver.com>
2022-06-12 16:32:28 +0900

    Add squeezeformer test function(#169)

---
+++ tests/test_acoustic_models/test_squeezeformer_transducer.py
@@ -0,0 +1,114 @@
+import unittest
+import torch
+import logging
+
+from openspeech.tokenizers.ksponspeech.character import KsponSpeechCharacterTokenizer
+from openspeech.criterion.transducer.transducer import TransducerLossConfigs
+from openspeech.models import SqueezeformerTransducerModel, SqueezeformerTransducerConfigs
+from openspeech.utils import (
+    DUMMY_INPUTS,
+    DUMMY_INPUT_LENGTHS,
+    DUMMY_TARGETS,
+    DUMMY_TARGET_LENGTHS,
+    build_dummy_configs,
+    WARPRNNT_IMPORT_ERROR,
+)
+
+try:
+    from warp_rnnt import rnnt_loss
+except ImportError:
+    raise ImportError(WARPRNNT_IMPORT_ERROR)
+
+logger = logging.getLogger(__name__)
+
+
+class TestConformerTransducer(unittest.TestCase):
+    def test_forward(self):
+        configs = build_dummy_configs(
+            model_configs=SqueezeformerTransducerConfigs(),
+            criterion_configs=TransducerLossConfigs(),
+        )
+
+        vocab = KsponSpeechCharacterTokenizer(configs)
+        model = SqueezeformerTransducerModel(configs, vocab)
+
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-04)
+
+        for i in range(3):
+            outputs = model(DUMMY_INPUTS, DUMMY_INPUT_LENGTHS)
+
+            loss = rnnt_loss(
+                outputs["logits"],
+                DUMMY_TARGETS,
+                outputs["encoder_output_lengths"],
+                DUMMY_TARGET_LENGTHS,
+                reduction="mean",
+                blank=vocab.blank_id,
+                gather=True,
+            )
+            loss.backward()
+            optimizer.step()
+            assert type(loss.item()) == float
+
+    def test_beam_search(self):
+        configs = build_dummy_configs(
+            model_configs=SqueezeformerTransducerConfigs(),
+            criterion_configs=TransducerLossConfigs(),
+        )
+
+        vocab = KsponSpeechCharacterTokenizer(configs)
+        model = SqueezeformerTransducerModel(configs, vocab)
+        model.set_beam_decode(beam_size=3)
+
+        for i in range(3):
+            prediction = model(DUMMY_INPUTS, DUMMY_INPUT_LENGTHS)["predictions"]
+            assert isinstance(prediction, torch.Tensor)
+
+    def test_training_step(self):
+        configs = build_dummy_configs(
+            model_configs=SqueezeformerTransducerConfigs(),
+            criterion_configs=TransducerLossConfigs(),
+        )
+
+        vocab = KsponSpeechCharacterTokenizer(configs)
+        model = SqueezeformerTransducerModel(configs, vocab)
+
+        for i in range(3):
+            outputs = model.training_step(
+                batch=(DUMMY_INPUTS, DUMMY_TARGETS, DUMMY_INPUT_LENGTHS, DUMMY_TARGET_LENGTHS), batch_idx=i
+            )
+            assert type(outputs["loss"].item()) == float
+
+    def test_validation_step(self):
+        configs = build_dummy_configs(
+            model_configs=SqueezeformerTransducerConfigs(),
+            criterion_configs=TransducerLossConfigs(),
+        )
+
+        vocab = KsponSpeechCharacterTokenizer(configs)
+        model = SqueezeformerTransducerModel(configs, vocab)
+
+        for i in range(3):
+            outputs = model.validation_step(
+                batch=(DUMMY_INPUTS, DUMMY_TARGETS, DUMMY_INPUT_LENGTHS, DUMMY_TARGET_LENGTHS), batch_idx=i
+            )
+            assert type(outputs["loss"].item()) == float
+
+    def test_test_step(self):
+        configs = build_dummy_configs(
+            model_configs=SqueezeformerTransducerConfigs(),
+            criterion_configs=TransducerLossConfigs(),
+        )
+
+        vocab = KsponSpeechCharacterTokenizer(configs)
+        model = SqueezeformerTransducerModel(configs, vocab)
+
+        for i in range(3):
+            outputs = model.test_step(
+                batch=(DUMMY_INPUTS, DUMMY_TARGETS, DUMMY_INPUT_LENGTHS, DUMMY_TARGET_LENGTHS), batch_idx=i
+            )
+            assert type(outputs["loss"].item()) == float
+
+
+if __name__ == '__main__':
+    unittest.main()

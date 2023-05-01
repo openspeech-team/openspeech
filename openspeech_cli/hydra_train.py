4c6e5aeb6b42d56e56ecf531a9d3e6a3acdabf76
upskyy <seomk9896@naver.com>
2023-05-01 21:16:45 +0900

    Fix segmentation fault error

--- openspeech_cli/hydra_train.py
+++ openspeech_cli/hydra_train.py
@@ -21,8 +21,8 @@
 # SOFTWARE.
 
 import os
-
 import hydra
+import sentencepiece
 import pytorch_lightning as pl
 from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning.utilities import rank_zero_info
